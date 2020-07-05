762bb9ab2a6f1448672ef37b7d89c5392ff1f452
han1202012 <octopus_truth@163.com>
2020-07-05 23:19:44 +0800

    update

--- app/src/main/java/kim/hsl/bp/MainActivity.java
+++ app/src/main/java/kim/hsl/bp/MainActivity.java
@@ -2,13 +2,22 @@ package kim.hsl.bp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Intent mAlarmIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    private void alarmKeep(){
+        mAlarmIntent = new Intent();
+
+    }
+
 }
